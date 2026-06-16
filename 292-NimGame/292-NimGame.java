1565c2df0faaca1ac2eff85fc036d839203663c5
puneetkumar52 <155947301+puneetkumar52@users.noreply.github.com>
2026-06-16 08:52:01 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 6/16/2026, 8:52:01 AM
+class Solution {
+    public boolean canWinNim(int n) {
+        if (n%4==0){
+            return false;
+        }
+        return true;
+    }
+}
